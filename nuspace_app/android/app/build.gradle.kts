15f75f4fa6c2b9baebd46eb86df3effe240f3355
akihiro24 <rkdv24@gmail.com>
2025-09-16 00:39:04 +0800

    1
    test

--- nuspace_app/android/app/build.gradle.kts
+++ nuspace_app/android/app/build.gradle.kts
@@ -37,18 +37,18 @@ android {
         applicationId = "com.nuspace.nuspace_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as? String ?: ""
+        keyPassword = keystoreProperties["keyPassword"] as? String ?: ""
+        storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+        storePassword = keystoreProperties["storePassword"] as? String ?: ""
         }
     }
 
